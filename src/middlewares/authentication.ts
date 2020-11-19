e7fd685e774190376d9c06489ba2d047427bced0
RalduginNA <fram.kiler@gmail.com>
2020-11-19 13:35:29 +0200

    Added env for testing fe

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -3,6 +3,24 @@ import jwt from '../helpers/jwt'
 import { STATUS_CODES } from '../constants/api'
 
 const authentication: Middleware = async (ctx, next) => {
+  // for testing fe
+  //---------------------
+  const {
+    TEST_USER_ID,
+    TEST_USER_NAME,
+    TEST_USER_EMAIL,
+    NODE_TEST_FE,
+  } = process.env
+  if (NODE_TEST_FE) {
+    ctx.state.user = {
+      _id: TEST_USER_ID,
+      username: TEST_USER_NAME,
+      email: TEST_USER_EMAIL,
+    }
+    await next()
+    return
+  }
+  ////---------------------
   try {
     const { _id, username, email } = await jwt.verifyAuthentication(
       ctx.header.authorization,
