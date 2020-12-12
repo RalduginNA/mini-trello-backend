cc8d536790452732e9d1818307cc21859df4f05f
RalduginNA <fram.kiler@gmail.com>
2020-12-12 23:20:49 +0200

    Some fixes

--- src/middlewares/authentication.ts
+++ src/middlewares/authentication.ts
@@ -3,12 +3,12 @@ import jwt from '../helpers/jwt'
 import { STATUS_CODES } from '../constants/api'
 
 // TODO: remove
-// for testing fe (temporary)
+// !for testing fe (temporary)
 import UserModel from '../modules/user/user.model'
 
 const authentication: Middleware = async (ctx, next) => {
-  // for testing fe
-  //---------------------
+  // !for testing fe
+  //!---------------------
   const { TEST_USER_NAME, NODE_TEST_FE } = process.env
   if (NODE_TEST_FE) {
     const user = await UserModel.findOne({ username: TEST_USER_NAME })
@@ -19,7 +19,7 @@ const authentication: Middleware = async (ctx, next) => {
     await next()
     return
   }
-  //-----------------------
+  //!----------------------
   try {
     ctx.state.user = await jwt.verifyAuthentication(ctx.header.authorization)
   } catch (err) {
--- src/types.ts
+++ src/types.ts
@@ -22,7 +22,7 @@ export interface Ctx<BodyT = {}, ParamsT = {}, StateT = CtxState>
   extends ParameterizedContext<StateT, RouterParamContext<StateT, {}>> {
   request: CtxRequest<BodyT>
   params: ParamsT
-  // body
+  //? body
 }
 
 export interface ParamsId {
