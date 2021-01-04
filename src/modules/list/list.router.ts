8f745c8199afbd9a3568abfa165dcb41408b2236
RalduginNA <fram.kiler@gmail.com>
2021-01-04 19:22:49 +0200

    refactor: some changes

--- src/modules/list/list.router.ts
+++ src/modules/list/list.router.ts
@@ -6,9 +6,7 @@ import validateRequest from '../../middlewares/validateRequest'
 const router = new Router({ prefix: `/lists` })
 
 router.post('/', validateRequest(ListSchema.create), ListController.create)
-
 router.put('/:id', validateRequest(ListSchema.update), ListController.update)
-
 router.delete('/:id', ListController.delete)
 
 export default router
