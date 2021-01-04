791c232ce624e63d134cbe41542a41a6bf3995e3
RalduginNA <fram.kiler@gmail.com>
2021-01-04 23:28:57 +0200

    refactor: update getMe controller

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,13 @@
-import { Ctx } from '../../types'
 import userModel from './user.model'
+import { STATUS_CODES } from '../../constants/api'
+import { Ctx } from '../../types'
 
-const getMe = async (ctx: Ctx<{}>) => {
+const getMe = async (ctx: Ctx) => {
   const { _id } = ctx.state.user
   const user = await userModel.findById(_id)
+
+  ctx.assert(user, STATUS_CODES.BAD_REQUEST, "User doesn't exist")
+
   ctx.body = user
 }
 
